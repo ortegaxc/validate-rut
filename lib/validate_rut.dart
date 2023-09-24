9e2e1f6b8b92121d55b6824984d304a21a5d109c
Tomas Antonio Ortega Orellana (CL) <tortegao@ripley.com>
2023-09-23 22:49:38 -0300

    refactor(validate_rut.dart): reorganize export statements in alphabetical order

--- lib/validate_rut.dart
+++ lib/validate_rut.dart
@@ -1,6 +1,7 @@
 library validate_rut;
 
 export 'src/calculate_dv.dart';
-export 'src/validate_rut.dart';
+export 'src/constants.dart';
 export 'src/format_rut.dart';
 export 'src/remove_rut_formatting.dart';
+export 'src/validate_rut.dart';
