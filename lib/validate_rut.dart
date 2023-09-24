8662d7d930ff006797d7497edcc690333c4616f0
Tomas Antonio Ortega Orellana (CL) <tortegao@ripley.com>
2023-09-24 12:24:05 -0300

    feat(validate_rut.dart): add rut_input_formatter.dart to exports

--- lib/validate_rut.dart
+++ lib/validate_rut.dart
@@ -4,4 +4,5 @@ export 'src/calculate_dv.dart';
 export 'src/constants.dart';
 export 'src/format_rut.dart';
 export 'src/remove_rut_formatting.dart';
+export 'src/rut_input_formatter.dart';
 export 'src/validate_rut.dart';
